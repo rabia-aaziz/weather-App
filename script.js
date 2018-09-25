6fd57a6f25ffc943aa616a6ec9d28f7e4423c682
rabia-aaziz <39001798+rabia-aaziz@users.noreply.github.com>
2018-09-25 23:58:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function getWeather() {
     function style(){
          document.querySelector("body").style["background-position"] = `bottom`;
-                document.querySelector("body").style["background-attachment"] = `fixed`;
+         document.querySelector("body").style["background-attachment"] = `fixed`;
     }
-
     document.querySelector(".info").style.display = "flex";
     const cityName = document.querySelector("input").value;
-
     $.ajax({
         url:
         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fb7296206e736a3c4c5c1e8718503389&units=metric`,
@@ -19,36 +17,22 @@ function getWeather() {
             document.querySelector(".min").innerHTML = data.main.temp_min;
             document.querySelector(".max").innerHTML = data.main.temp_max;
             document.querySelector("#icon").innerHTML = "<img  class = 'bg-img' src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>";
-
             let time = new Date();
             let sunrise = new Date(data.sys.sunrise * 1000);
             let sunset = new Date(data.sys.sunset * 1000);
-
-// ----------------------i should do these changes-------------------------------------------
-            // document.querySelector("body").style["background-position"] = `bottom`;
-            // document.querySelector("body").style["background-attachment"] = `fixed`;
-// ---------------------------------------------------------------------------------------------
             if (time > sunrise && time < sunset) {
                 document.querySelector("body").style["background-image"] = `url(images/day.jpg)`;
                 style();
-//                 document.querySelector("body").style["background-position"] = `bottom`;
-//                 document.querySelector("body").style["background-attachment"] = `fixed`;
                 document.querySelector("body").style.color = `black`;
             } else {
                 document.querySelector("body").style["background-image"] = `url(images/night.png)`;
-//                 document.querySelector("body").style["background-position"] = `bottom` ;
-//                 document.querySelector("body").style["background-attachment"] = `fixed`;
                 style();
                 document.querySelector("body").style.color = `white`;
             }
-
         },
         error: function (err) {
-
             document.querySelector("body").style["background-image"] = `url(images/4o4.jpg)`;
             document.querySelector("body").style[" background-size"] = ` cover`;
-//             document.querySelector("body").style[" background-position"] = `bottom`;
-//             document.querySelector("body").style[" background-attachement"] = `fixed`;
             style();
             document.querySelector(".info").style.display = "none";
 
