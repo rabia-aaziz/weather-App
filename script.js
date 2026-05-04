54716d6a42d0d6b795e9706646837e7ec2f2a8a4
rabia-aaziz <39001798+rabia-aaziz@users.noreply.github.com>
2026-05-04 14:30:21 +0500

    Update script.js
    just changes scrip.js

--- script.js
+++ script.js
@@ -1,46 +1,73 @@
+// function getWeather() {
+//     function style(){
+//          document.querySelector("body").style["background-position"] = `bottom`;
+//          document.querySelector("body").style["background-attachment"] = `fixed`;
+//     }
+//     document.querySelector(".info").style.display = "flex";
+//     const cityName = document.querySelector("input").value;
+//     $.ajax({
+//         url:
+//         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fb7296206e736a3c4c5c1e8718503389&units=metric`,
+//         success: function (data) {
+//             console.log(data);
+//             // setInnerHtml(".city",data.name);
+//             document.querySelector(".city").innerHTML = data.name;
+//             document.querySelector(".temp>span").innerHTML = data.main.temp;
+//             document.querySelector(".description").innerHTML = data.weather[0].main;
+//             document.querySelector(".min").innerHTML = data.main.temp_min;
+//             document.querySelector(".max").innerHTML = data.main.temp_max;
+//             document.querySelector("#icon").innerHTML = "<img  class = 'bg-img' src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>";
+//             let time = new Date();
+//             let sunrise = new Date(data.sys.sunrise * 1000);
+//             let sunset = new Date(data.sys.sunset * 1000);
+//             if (time > sunrise && time < sunset) {
+//                 document.querySelector("body").style["background-image"] = `url(images/day.jpg)`;
+//                 style();
+//                 document.querySelector("body").style.color = `black`;
+//             } else {
+//                 document.querySelector("body").style["background-image"] = `url(images/night.png)`;
+//                 style();
+//                 document.querySelector("body").style.color = `white`;
+//             }
+//         },
+//         error: function (err) {
+//             document.querySelector("body").style["background-image"] = `url(images/4o4.jpg)`;
+//             document.querySelector("body").style[" background-size"] = ` cover`;
+//             style();
+//             document.querySelector(".info").style.display = "none";
+
+
+//         },
+//         // setInnerHtml: function(querySelector, setValue){
+//         //     document.querySelector(querySelector).innerHTML = setValue;
+//         // }
+//     });
+// }
+
+
+// new code---―
 function getWeather() {
-    function style(){
-         document.querySelector("body").style["background-position"] = `bottom`;
-         document.querySelector("body").style["background-attachment"] = `fixed`;
-    }
     document.querySelector(".info").style.display = "flex";
-    const cityName = document.querySelector("input").value;
+
+    const cityName = document.getElementById("user-Input").value.trim();
+
     $.ajax({
-        url:
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fb7296206e736a3c4c5c1e8718503389&units=metric`,
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fb7296206e736a3c4c5c1e8718503389&units=metric`,
+
         success: function (data) {
-            console.log(data);
-            // setInnerHtml(".city",data.name);
             document.querySelector(".city").innerHTML = data.name;
-            document.querySelector(".temp>span").innerHTML = data.main.temp;
+            document.querySelector(".temp span").innerHTML = data.main.temp;
             document.querySelector(".description").innerHTML = data.weather[0].main;
             document.querySelector(".min").innerHTML = data.main.temp_min;
             document.querySelector(".max").innerHTML = data.main.temp_max;
-            document.querySelector("#icon").innerHTML = "<img  class = 'bg-img' src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>";
-            let time = new Date();
-            let sunrise = new Date(data.sys.sunrise * 1000);
-            let sunset = new Date(data.sys.sunset * 1000);
-            if (time > sunrise && time < sunset) {
-                document.querySelector("body").style["background-image"] = `url(images/day.jpg)`;
-                style();
-                document.querySelector("body").style.color = `black`;
-            } else {
-                document.querySelector("body").style["background-image"] = `url(images/night.png)`;
-                style();
-                document.querySelector("body").style.color = `white`;
-            }
-        },
-        error: function (err) {
-            document.querySelector("body").style["background-image"] = `url(images/4o4.jpg)`;
-            document.querySelector("body").style[" background-size"] = ` cover`;
-            style();
-            document.querySelector(".info").style.display = "none";
 
-
+            document.querySelector("#icon").innerHTML =
+            `<img class="bg-img" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
         },
-        // setInnerHtml: function(querySelector, setValue){
-        //     document.querySelector(querySelector).innerHTML = setValue;
-        // }
+
+        error: function () {
+            alert("City not found");
+        }
     });
 }
 
